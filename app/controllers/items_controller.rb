266e6f6ee09792c74a3b14a9b62c0ac15922867c
Isabel Cheung <isabelczyi@gmail.com>
2022-03-07 13:51:03 +1000

    controller and views for item

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,28 @@
+class ItemsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def updated
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+  end
+end
