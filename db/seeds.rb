e4317d945470b5c3c8250542d0f28d8b68200ac4
Sarah van der Waal <svanderwaal@hotmail.com>
2022-03-08 06:41:16 +1100

    added seeding code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+p "Deleting previous data..."
+# destroy data
+User.destroy_all if Rails.env == 'development'
+List.destroy_all if Rails.env == 'development'
+Item.destroy_all if Rails.env == 'development'
+Location.destroy_all if Rails.env == 'development'
+
+p "Seeding data.."
+
+# add user data
+5.times do
+  User.create(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password: 'password'
+  )
+end
+
+p "Created #{User.count} users..."
+
+# add list data
+25.times do
+  List.create(
+    name: Faker::Lorem.sentence(word_count: 3),
+    user_id: rand(1..5)
+  )
+end
+
+p "Created #{List.count} lists.."
+
+# add item data
+50.times do
+  Item.create(
+    name: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.sentences,
+    list_id: rand(1..25),
+    completed: Faker::Boolean.boolean(true_ratio: 0.2),
+    user_id: rand(1..25)
+  )
+end
+
+p "Created #{List.count} lists.."
+
+# add location data
+25.times do
+  Location.create(
+    address: Faker::Address.city,
+    item_id:
+  )
+end
+
+p "Created #{Location.count} locations.."
