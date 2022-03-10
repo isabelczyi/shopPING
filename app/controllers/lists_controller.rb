aca0b44823ff03abff60e7966aa879b56fed2a31
Sarah van der Waal <svanderwaal@hotmail.com>
2022-03-10 11:04:31 +1100

    added link to item show page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,8 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     authorize @list
+    @item = Item.find(params[:id])
+    authorize @item
   end
 
   def new
