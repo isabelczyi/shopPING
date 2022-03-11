e643df797e1ae57ed215edd2648ae346f9067936
Monika Zuscak <monikazuscak@gmail.com>
2022-03-11 13:44:31 +1100

    added design for list item list page buttons and add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :components ]
 
   def home
     @user = current_user
+  end
+
+  def components
   end
 end
