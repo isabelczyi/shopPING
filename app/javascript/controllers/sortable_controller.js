b818b2f55ea48df96c523f1ff4ef0b5fb2d70404
Padmini Hiriyur <padmini.hm@gmail.com>
2022-03-15 11:25:52 +1100

    built the sortable functionality for items

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,7 +1,32 @@
+import { Controller } from "stimulus"
 import Sortable from "sortablejs"
 
 const list = document.querySelector("#enablesortable")
+export default class extends Controller {
+static targets = ["list"]
 
-const initSortable = () => {
-  Sortable.create(list)
+  connect() {
+    console.log(this.listTarget);
+    this.initSortable()
+  }
+  initSortable() {
+    Sortable.create(this.listTarget, {
+      draggable: ".sortable-draggable",
+      ghostClass: "ghost",
+      animation: 150,
+      delay: 100, // time in milliseconds to define when the sorting should start
+	    delayOnTouchOnly: false,
+      onEnd: this.updateItemPosition.bind(this)
+    })
+  }
+
+  updateItemPosition(evt) {
+    console.log("Call api to update position")
+    console.log('old position', evt.oldIndex)
+    console.log('new position', evt.newIndex)
+    // fetch to update position of item (update item path)
+    // hand in the new position + 1 as the new position
+    // once successful, cool haha
+
+  }
 }
